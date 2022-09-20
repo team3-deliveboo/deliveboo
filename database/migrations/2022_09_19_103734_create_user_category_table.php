9ff9c8a9e41e260127d1828afbd93643f2f936a7
SAX_94 <matteosacchi94@gmail.com>
2022-09-20 09:07:29 +0200

    Added useful comments on migrations

--- database/migrations/2022_09_19_103734_create_user_category_table.php
+++ database/migrations/2022_09_19_103734_create_user_category_table.php
@@ -40,3 +40,41 @@ class CreateUserCategoryTable extends Migration
         Schema::dropIfExists('user_category');
     }
 }
+
+// DALLA REPO DEGLI ALTRI, CI MANCAVA UN PEZZO PURE IN QUESTA
+
+// // class CreateCategoryUserTable extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         Schema::create('category_user', function (Blueprint $table) {
+//             $table->unsignedBigInteger('user_id');
+//             $table->foreign('user_id')
+//                 ->references('id')
+//                 ->on('users');
+
+//             $table->unsignedBigInteger('category_id');
+//             $table->foreign('category_id')
+//                 ->references('id')
+//                 ->on('categories');
+
+//             $table->primary(['user_id', 'category_id']);
+//             $table->timestamps();
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         Schema::dropIfExists('category_user');
+//     }
+// }
--- database/migrations/2022_09_19_144444_create_dish_order_table.php
+++ database/migrations/2022_09_19_144444_create_dish_order_table.php
@@ -41,3 +41,42 @@ class CreateDishOrderTable extends Migration
         Schema::dropIfExists('dish_order');
     }
 }
+
+// DALLA REPO DEGLI ALTRI, CI MANCAVA UN PEZZO:
+
+// class CreateDishOrderTable extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         Schema::create('dish_order', function (Blueprint $table) {
+//             $table->unsignedBigInteger('dish_id');
+//             $table->foreign('dish_id')
+//                 ->references('id')
+//                 ->on('dishes');
+
+//             $table->unsignedBigInteger('order_id');
+//             $table->foreign('order_id')
+//                 ->references('id')
+//                 ->on('orders');
+
+//             $table->primary(['dish_id', 'order_id']);
+//             $table->tinyInteger('quantity')->default(1);
+//             $table->timestamps();
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         Schema::dropIfExists('dish_order');
+//     }
+// }
