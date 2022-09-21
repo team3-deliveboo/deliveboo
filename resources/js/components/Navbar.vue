3778c5785d27af8a58e527b47374cd5f34616c82
silviawolves <silviacanepa.w@gmail.com>
2022-09-21 17:51:38 +0200

    initial vue implementation

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style lang="scss" scoped>
+@import './resources/sass/_variables.scss';
+
+.container {
+    background-color: $deliveroo-blue;
+    height: 60px;
+}
+
+</style>
