edb8d43236a43c4455844e2c52ae86c945be51d6
MatteoLoddo <loddomatteo96@gmail.com>
2022-09-24 18:56:05 +0200

    commit fix
    Co-authored-by: Marius Leica <marius-leica@users.noreply.github.com>
    Co-authored-by: Matteo Sacchi <SAX-94@users.noreply.github.com>

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -19035,7 +19035,7 @@ var routes = [{
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Documents\Boolean\deliveboo\resources\js\frontend.js */"./resources/js/frontend.js");
+module.exports = __webpack_require__(/*! D:\RepositoryBoolean\deliveboo\resources\js\frontend.js */"./resources/js/frontend.js");
 
 
 /***/ })
