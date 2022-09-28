b8efa6aeea35a7c035f91d8178941963502d8209
Marius L <mariusleica@gmail.com>
2022-09-28 17:44:19 +0200

    first section category list
    Co-authored-by: Matteo Sacchi <SAX-94@users.noreply.github.com>
    Co-authored-by: MatteoLoddo <MatteoLoddo@users.noreply.github.com>
    Co-authored-by: fracabu <fracabu@users.noreply.github.com>

--- resources/js/frontend/components/FirstSection.vue
+++ resources/js/frontend/components/FirstSection.vue
@@ -2,38 +2,93 @@
     <div class="restaurant-section">
         <div class="container">
             <div class="row gy-3">
+                <div class="form-check">
+                    <div class="col-4" v-for="category in categories" :key="category.id">
+                        <input class="form-check-input" type="checkbox" v-model="selected" :value="category.name"
+                            :id="category.id">
+                        <label class="form-check-label" for="flexCheckIndeterminateDisabled">{{ category.name }}</label>
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-primary">
+                            Apply filter
+                        </button>
+                    </div>
+                </div>
+                <!-- <div class="col-4" v-for="category in categories" :key="category.id"></div>
+                    {{ category.name }}
+                </div> -->
                 <div class="col-4" v-for="restaurant in restaurants" :key="restaurant.id">
                     <router-link class="text-dark text-decoration-none" :to="{
                         name: 'users.show',
                         params: { slug: restaurant.slug },
                     }">
-                        <div class="card-restaurant">
+                        <div class="card-restaurant d-flex flex-column">
                             <div class="restaurant-img">
                                 <img :src="getImg(restaurant)" alt="/" />
                             </div>
-                            <div class="restaurant-name"></div>
-                            {{ restaurant.name }}
-                        </div>
+                            <div class="restaurant-name">
+                                {{ restaurant.name }}
+                            </div>
 
-                        <div>
-                            {{ restaurant.categories }}
-                        </div>
-                        <div v-if='restaurant.categories'>
-                            <div v-for="category in restaurant.categories" :key="category.id">
-                                {{ category.name }}
+
+                            <!-- <div class="col-4" v-for="restaurant in filterData" :key="restaurant.id">
+                    <router-link class="text-dark text-decoration-none" :to="{
+                      name: 'users.show',
+                      params: { slug: restaurant.slug },
+                    }">
+                        <div class="card-restaurant d-flex flex-column">
+                            <div class="restaurant-img">
+                                <img :src="getImg(restaurant)" alt="/" />
+                            </div>
+                            <div class="restaurant-name">
+                                {{ restaurant.name }}
+                            </div> -->
+
+                            <!-- category -->
+                            <div v-if="restaurant.categories">
+                                <span v-for="category in restaurant.categories" :key="category.id">
+                                    <span>{{ category.name }}</span>
+                                </span>
                             </div>
                         </div>
                     </router-link>
                 </div>
-
-            </div>
-            <div v-for="category in restaurants.categories" :key="category.id">
-                {{ category.name }}
             </div>
         </div>
     </div>
-    </div>
 </template>
+<!-- 
+<h4>Filtra per categorie</h4>
+<div class="row">
+        <div
+            class="col-12 d-flex justify-content-start"
+            style="flex-wrap: wrap"
+        >
+            <div
+                v-for="(category, index) in categories"
+                :key="`category-${index}`"
+                class="shadow p-3 mb-2 rounded d-flex align-items-center justify-content-center mx-2 my-1 checkbox-category"
+            >
+                <input
+                    type="checkbox"
+                    name="categories[]"
+                    :value="category.name"
+                    v-model="form.categories"
+                    @change.prevent="
+                        getRestaurants(
+                            `${url}restaurants/searchCheck`,
+                            form
+                        )
+                    "
+                />
+                <label
+                    class="category-name"
+                    :for="category.name"
+                    >{{ category.name }}</label
+                >
+            </div>
+        </div>
+    </div> -->
 
 <script>
 import axios from "axios";
@@ -42,10 +97,36 @@ export default {
     name: "FirstSection",
     data() {
         return {
+            selected: [],
             restaurants: [],
+            categories: [],
+            nuovoArrayRistoranti: [],
         };
     },
     methods: {
+        // result(str) {
+        //     str.match('\w\s\w').join(separator)
+        // },
+
+        selectCat() {
+            for (let i = 0; i < this.restaurants.length; i++) {
+                for (let i = 0; i < categories.length; i++) {
+                    restaurant.categories.includes(category);
+
+                }
+            }
+        },
+
+        result(str) {
+            str.match(/[a-z]+|[^a-z]+/gi).join(this.separator);
+            // str.replace(/\s/g, " ");
+        },
+        fetchCategories() {
+            axios.get("/api/categories").then((resp) => {
+                this.categories = resp.data;
+            });
+        },
+
         fetchUsers() {
             axios.get("/api/users").then((resp) => {
                 console.log(resp);
@@ -58,10 +139,17 @@ export default {
             }
             return "/storage/" + restaurant.img;
         },
+        // funzioni cecklist
+        selectCategories() {
+            this.nuovoArrayRistoranti = map(this.restaurants);
+            console.log(selectCatecories);
+        }
+
+
     },
     mounted() {
+        this.fetchCategories();
         this.fetchUsers();
-        console.log(this.categories);
     },
 };
 </script>
