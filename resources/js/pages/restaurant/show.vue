1fcd5f5fa316c30875a06a37692e13cb2af76e12
silviawolves <silviacanepa.w@gmail.com>
2022-09-29 18:19:23 +0200

    cart styled

--- resources/js/pages/restaurant/show.vue
+++ resources/js/pages/restaurant/show.vue
@@ -25,104 +25,117 @@
 
             <!-- DISH + CART -->
             <div class="container">
-            <div class="row w-100">
+                <div class="row w-100">
 
-                <!-- DISHES LIST -->
-                <div class="col-8">
-                    <div class="dishes-section">
+                    <!-- DISHES LIST -->
+                    <div class="col-8">
+                        <div class="dishes-section">
 
-                        <h4 class="fw-bold pb-0 mb-0">Menù</h4>
-                        <div class="pb-3 text-muted">Scegli il tuo piatto, ed aggiungilo al carrello.</div>
+                            <h4 class="fw-bold pb-0 mb-0">Menù</h4>
+                            <div class="pb-3 text-muted">Scegli il tuo piatto, ed aggiungilo al carrello.</div>
 
-                        <!-- SINGLE DISH CARD -->
-                        <div class="row row-cols-2 g-3">
-
-                            <div class="col" v-for="dish in restaurant.dishes" :key="dish.id">
-                                    <button @click="addItemToCart(dish)">
-                                        <div class="dish-card">
-                                            <div class="text-start pe-1">
-                                                <div><b>{{ dish.name }}</b></div>
-                                                <div class="description">{{ dish.description }}</div>
-                                                <div>{{ dish.price + "€" }}</div>
+                            <!-- SINGLE DISH CARD -->
+                            <div class="row row-cols-2 g-3 w-100">
+                                <div class="col" v-for="dish in restaurant.dishes" :key="dish.id">
+                                        <button @click="addItemToCart(dish)">
+                                            <div class="dish-card">
+                                                <div class="text-start pe-1">
+                                                    <div><b>{{ dish.name }}</b></div>
+                                                    <div class="description">{{ dish.description }}</div>
+                                                    <div>{{ dish.price + "€" }}</div>
+                                                </div>
+                                                <div class="dish-img">
+                                                    <img :src="'/storage/' + dish.img" :alt="'Immagine del piatto' + ' ' + dish.name" />
+                                                </div>
                                             </div>
-                                            <div class="dish-img">
-                                                <img :src="'/storage/' + dish.img" :alt="'Immagine del piatto' + ' ' + dish.name" />
+                                        </button>
+                                        <!-- <button @click="addItemToCart(dish)">add</button> -->
+                                    <!-- <button @click="convertJson()" ></button> -->
+                                </div>
+                            </div>
+                            <!-- <CartComp></CartComp> -->
+                        </div>
+                    </div>
+
+                <!-- <div v-for="item in cart" :key="item.id">
+                    <span>{{item.name}}</span>
+                    <span>{{item.price + "€" }}</span> -->
+
+
+                    <!-- CART SECTION -->
+                    <div class="col-4">
+                        <div class="py-4 h-100">
+                            <div class="cart-section">
+
+                                <div v-if="cart.length <= 0" class="empty-cart">
+                                    <i class="fa-solid fa-basket-shopping"></i>
+                                    <div class="empty">Il carrello è vuoto</div>
+                                </div>
+
+                                <template v-if="cart.length > 0">
+                                    <h5 class="pb-3 fw-bold">Il tuo ordine</h5>
+                                </template>
+
+                                <div v-for="dish in cart" :key="dish.id" class="row">
+
+                                    <!-- SELECTED DISH -->
+                                    <div class="col-3">
+                                        <p>{{ dish.name }}</p>
+                                    </div>
+
+                                    <!-- QUANTITY BUTTONS -->
+                                    <div class="col-3">
+                                        <div class="d-flex align-items-center cart-quantity-button">
+                                            <div class="pill-button">
+                                                <a @click="removeOneFromCart(dish)" class="no-decoration">
+                                                    <i class="fa-regular fa-square-minus"></i>
+                                                </a>
+                                            </div>
+
+                                            <div class="display-num-pill-button">
+                                                {{ dish.quantity }}
+                                            </div>
+
+                                            <div>
+                                                <a @click="addItemToCart(dish)" class="no-decoration">
+                                                    <i class="fa-regular fa-square-plus"></i>
+                                                </a>
                                             </div>
                                         </div>
-                                    </button>
-                                    <!-- <button @click="addItemToCart(dish)">add</button> -->
-                                
-                                <!-- <button @click="convertJson()" ></button> -->
-                            </div>
+                                    </div>
 
-                        </div>
+                                    <!-- PRICE-->
+                                    <div class="col-3">
+                                        <p class="ps-1 price text-nowrap">{{ (dish.price * dish.quantity).toFixed(2) }} €</p>
+                                    </div>
 
-                        <!-- <CartComp></CartComp> -->
-                    </div>
-                </div>
-
-            <!-- <div v-for="item in cart" :key="item.id">
-                <span>{{item.name}}</span>
-                <span>{{item.price + "€" }}</span> -->
-
-
-                <!-- CART SECTION -->
-                <div class="col-4">
-                    <div class="py-4 h-100">
-                    <div class="cart-section">
-
-                        <h5 class="pb-3 fw-bold">Il tuo ordine</h5>
-                    
-                        <div v-for="dish in cart" :key="dish.id" class="row dish-container">
-
-                            <div class="col">
-                                <div class="dish-desc">{{ dish.description }}</div>
-                            </div>
-
-                            <div class="col dish-information d-flex">
-
-                                <div class="dish-and-price d-flex">
-                                    <p class="text-orange">{{ dish.name }}</p>
-                                    <p class="ps-1 price text-nowrap">
-                                        € {{ (dish.price * dish.quantity).toFixed(2) }}</p>
-                                </div>
-                                
-                            </div>
-
-                            <div class="col">
-                                <div class="d-flex align-items-center cart-quantity-button ps-4">
-
-                                    <div>
-                                        <!-- bin icon -->
+                                    <!-- TRASHCAN -->
+                                    <div class="col-3">
                                         <a class="no-decoration" @click="removeAllFromCart(dish)"><i class="fa-solid fa-trash"></i></a>
                                     </div>
 
-                                    <!-- add and remove item from cart  -->
-                                    <div class="pill-button">
-                                        <a @click="removeOneFromCart(dish)" class="no-decoration">-</a>
-                                    </div>
-
-                                        <div class="display-num-pill-button">
-                                            {{ dish.quantity }}
-                                        </div>
-
-                                        <div>
-                                        <a @click="addItemToCart(dish)" class="no-decoration">+</a>
-                                    </div>
-
                                 </div>
+
+                                <!-- PAYMENT BUTTON -->
+                                <div class="payment-section d-flex justify-content-center">
+                                    <button class="grey-btn position-absolute" style="bottom: 20px; width: 90%;">
+                                        <b>Vai al pagamento</b>
+                                    </button>
+
+                                    <template v-if="cart.length > 0">
+                                        <button class="blue-btn position-absolute" style="bottom: 20px; width: 90%;">
+                                            <b>Vai al pagamento</b>
+                                        </button>
+                                    </template>
+                                </div>
+
                             </div>
-
-                            
-
                         </div>
-                    </div>
-                    </div>
 
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
 
 
         <!-- VUE COMPONENT: FOOTER -->
@@ -379,49 +392,84 @@ export default {
 
         .dishes-section {
             padding: 2rem 0;
+            overflow-y: auto;
+            width: 100%;
 
             button {
-                    width: 100%;
-                    border: none;
+                width: 100%;
+                border: none;
+                border-radius: 5px;
+                background-color: transparent;
+
+            .dish-card {
+                width: 100%;
+                background-color: white;
+                border: 1px solid white;
+                border-radius: 5px;
+                padding: 1rem;
+                display: flex;
+                justify-content: space-between;
+                box-shadow: 0 0 4px rgb(224, 224, 224);
+
+                .dish-img img {
+                    width: 100px;
+                    aspect-ratio: 1/1;
                     border-radius: 5px;
-                    background-color: transparent;
+                    border: 1px solid $deliveroo-grey;
+                }
 
-                .dish-card {
-                    width: 100%;
-                    background-color: white;
-                    border: 1px solid white;
-                    border-radius: 5px;
-                    padding: 1rem;
-                    display: flex;
-                    justify-content: space-between;
-                    box-shadow: 0 0 4px rgb(224, 224, 224);
-
-                    .dish-img img {
-                        width: 100px;
-                        aspect-ratio: 1/1;
-                        border-radius: 5px;
-                        border: 1px solid $deliveroo-grey;
-                    }
-
-                    .description {
-                        font-size: .85rem;
-                        line-height: 130%;
-                        opacity: 75%;
-                        padding: .5rem 0 .7rem 0;
-                        text-overflow: ellipsis;
-                    }
+                .description {
+                    font-size: .85rem;
+                    line-height: 130%;
+                    opacity: 75%;
+                    padding: .5rem 0 .7rem 0;
+                    text-overflow: ellipsis;
                 }
             }
         }
+    }
 
         .cart-section {
             height: 500px;
             width: 100%;
             background-color: white;
             border: 1px solid white;
-            padding: 2rem 0 4rem 2rem;
-            overflow: auto;
+            padding: 2rem 2rem 4rem 2rem;
+            overflow-y: auto;
             box-shadow: 0 0 4px rgb(224, 224, 224);
+            position: relative;
+            
+            .empty-cart {
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+                gap: 1rem;
+
+                & i {
+                    color: #abadad;
+                    font-size: 50px;
+                }
+
+                .empty {
+                    color: #abadad;
+                }
+            }
+
+            a i {
+                color: $deliveroo-blue;
+                margin: 0 .5rem;
+                cursor: pointer;
+            }
+
+            .payment-section {
+                height: 150px;
+                width: 100%;
+                background-color: white;
+                box-shadow: 0 -5px 5px -2px rgb(224, 224, 224);
+                position: absolute;
+                bottom: 0;
+                left: 0;
+            }
         }
     }
 }
