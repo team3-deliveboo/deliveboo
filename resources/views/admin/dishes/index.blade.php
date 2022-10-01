3a6eb07c8aa39471b1b0c66806cdd5f3b80affff
silviawolves <silviacanepa.w@gmail.com>
2022-10-01 18:43:01 +0200

    styled admin dish index

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,84 +1,56 @@
 @extends ('layouts.app')
 
 @section('content')
-    <a href="{{ route('admin.dishes.create') }}" class="btn btn-primary mb-3">Crea nuovo piatto</a>
-    <table class="table align-middle">
-        <thead class="text-center">
-            <th>Nome Piatto</th>
-            <th>Portata</th>
-            <th>Descrizione</th>
-            <th>Prezzo</th>
+    <div class="dish-index">
 
-            <th>Azioni</th>
-        </thead>
-        <tbody>
-            @if (Session::has('status'))
-                <div class="alert alert-success" role="alert">
-                    <p>{{ Session::get('status') }}</p>
-                </div>
-            @endif
+        <div class="button-create">
+            <a href="{{ route('admin.dishes.create') }}" class="blue-btn">Crea nuovo piatto</a>
+        </div>
 
+
+        @if (Session::has('status'))
+            <div class="alert alert-success" role="alert">
+                <p>{{ Session::get('status') }}</p>
+            </div>
+        @endif
+
+
+        {{-- DISH CARD --}}
+        <div class="row gy-3 justify-content-center gap-5">
             @foreach ($dishes as $dish)
-                <tr class="text-center">
-                    <td>{{ ucfirst($dish->name) }}</td>
-                    <td>{{ ucwords($dish->course) }}</td>
-                    <td>{{ ucfirst($dish->description) }}</td>
-                    <td>{{ $dish->price }} €</td>
+                <div class="col-5">
+                    <div class="dish-card position-relative">
+                        <div class="text-start pe-1">
+                            <div>
+                                <b>{{ ucfirst($dish->name) }}</b>
+                            </div>
+                            <div class="description">{{ ucfirst($dish->description) }}</div>
+                            <div>{{ $dish->price }} €</div>
+                        </div>
+                        <div class="dish-img">
+                            <img src="{{ asset('storage/' . $dish->img) }}" alt="{{ 'Immagine del piatto' . $dish->name }}" />
+                        </div>
 
-                    <td class="text-center">
-                        <a class="btn btn-warning btn-sm" href="{{ route('admin.dishes.edit', $dish->slug) }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="black" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-activity">
-                                <polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
-                                <line x1="3" y1="22" x2="21" y2="22">
-                                </line>
-                            </svg>
-                        </a>
-                    
+                        <div class="buttons">
+                            {{-- EDIT BUTTON --}}
+                            <a class="btn" href="{{ route('admin.dishes.edit', $dish->slug) }}">
+                                <i class="fa-solid fa-pen"></i>
+                            </a>
 
-                    
-                        <a class="btn btn-primary btn-sm" href="{{ route('admin.dishes.show', $dish->slug) }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="black" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-activity">
-                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                                <circle cx="12" cy="12" r="3"></circle>
-                            </svg>
-                        </a>
-                    
-
-                    
-                        <form class="d-inline-block delete" action="{{ route('admin.dishes.destroy', $dish->slug) }}"
-                            method="post">
+                            {{-- DELETE BUTTON --}}
+                            <form class="delete" action="{{ route('admin.dishes.destroy', $dish->slug) }}" method="post">
                             @csrf
                             @method('DELETE')
+                                <button type="submit" class="btn" onclick="return confirm('Sei sicuro di voler cancellare questo piatto?')">
+                                    <i class="fa-solid fa-ban"></i>
+                                </button>
+                            </form>
 
-                            <button type="submit" class="btn btn-danger"
-                                onclick="return confirm('Sei sicuro di voler cancellare questo piatto?')">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black"
-                                    class="bi bi-trash" viewBox="0 0 16 16">
-                                    <path
-                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                    <path fill-rule="evenodd"
-                                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                                </svg>
-                            </button>
-                            {{-- <button type="submit" class="btn btn-danger btn-sm">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-trash" viewBox="0 0 16 16">
-                                    <path
-                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-                                    <path fill-rule="evenodd"
-                                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
-                                </svg>
-                            </button> --}}
-                        </form>
-                    </td>
-
-                </tr>
+                        </div>
+                    </div>
+                </div>
             @endforeach
-        </tbody>
-    </table>
+        </div>
+    </div>
 @endsection
 
