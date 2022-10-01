2b100717c42c2a4fa37f34d475800ad2e8d85f06
silviawolves <silviacanepa.w@gmail.com>
2022-10-01 18:46:40 +0200

    admin dish show edited

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,13 +1,24 @@
 @extends ('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>{{ $dish->name }}</h1>
-        <h2>{{ $dish->description }}</h2>
-        <h2>{{ $dish->course }}</h2>
-        <h3>{{ $dish->price }} €</h3>
-        <h3>{{ $dish->visibility == 1 ? 'Disponibile' : 'Non disponibile' }}</h3>
-        <img class="img-fluid" src="{{ $dish->img ? asset('storage/' . $dish->img) : asset('storage/' . 'placeholder.webp')  }}">
+    <div class="container h-100 d-flex align-items-center">
 
+        {{-- DISH CARD --}}
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <div class="dish-card position-relative">
+                    <div class="text-start pe-1">
+                        <div>
+                            <b>{{ ucfirst($dish->name) }}</b>
+                        </div>
+                        <div class="description">{{ ucfirst($dish->description) }}</div>
+                        <div>{{ $dish->price }} €</div>
+                    </div>
+                    <div class="dish-img">
+                        <img src="{{ asset('storage/' . $dish->img) }}" alt="{{ 'Immagine del piatto' . $dish->name }}" />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
